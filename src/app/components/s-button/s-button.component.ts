bb14248d0f11beb1a3edd4a0d108d98b5d836ceb
Sony <jsaulhernandez98@gmail.com>
2023-07-05 20:05:06 -0600

    tag: adding component button

---
+++ src/app/components/s-button/s-button.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ThemeTypeIcon, TypeButton } from 'src/app/constants/constants';
+
+@Component({
+    selector: 'app-s-button',
+    templateUrl: './s-button.component.html',
+    styleUrls: ['./s-button.component.scss'],
+})
+export class SButtonComponent {
+    public buttonText = 'BUTTON TEXT';
+
+    @Input()
+    set text(name: string) {
+        this.buttonText = name.toUpperCase();
+    }
+
+    @Input() type: TypeButton = 'primary';
+    @Output() btnClick? = new EventEmitter();
+    @Input() isDisabled = false;
+    @Input() isLoading = false;
+    @Input() isDanger = false;
+    @Input() prefix: string = '';
+    @Input() suffix: string = '';
+    @Input() themeIcon: ThemeTypeIcon = 'outline';
+
+    constructor() {}
+
+    get name(): string {
+        return this.buttonText;
+    }
+
+    onClick() {
+        this.btnClick && this.btnClick.emit();
+    }
+}
