978540c139a2c3dca82b2b90872d17bbe0e94a60
Sony <jsaulhernandez98@gmail.com>
2023-07-06 19:29:55 -0600

    tag: adding input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import en from '@angular/common/locales/es';
+
+import { NZ_I18N, es_ES } from 'ng-zorro-antd/i18n';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        SharedModule,
+    ],
+    providers: [{ provide: NZ_I18N, useValue: es_ES }],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/s-input/s-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-s-input',
+  templateUrl: './s-input.component.html',
+  styleUrls: ['./s-input.component.scss']
+})
+export class SInputComponent {
+
+}
