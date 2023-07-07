e1e6566a95e51d40a2bc37c6a979a5641d4f650b
Sony <jsaulhernandez98@gmail.com>
2023-07-07 17:25:18 -0600

    tag: updating input component

--- src/app/components/s-input/s-input.component.ts
+++ src/app/components/s-input/s-input.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
-  selector: 'app-s-input',
-  templateUrl: './s-input.component.html',
-  styleUrls: ['./s-input.component.scss']
+    selector: 'app-s-input',
+    templateUrl: './s-input.component.html',
+    styleUrls: ['./s-input.component.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => SInputComponent),
+            multi: true,
+        },
+    ],
 })
-export class SInputComponent {
+export class SInputComponent implements ControlValueAccessor {
+    onChange: any = () => {};
+    onTouch: any = () => {};
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+    registerOnTouched(fn: any): void {
+        this.onTouch = fn;
+    }
 
+    input?: string;
+    writeValue(input: string) {
+        this.input = input;
+    }
+
+    @Input() typeInput: 'text' | 'password' = 'text';
+    @Input() isFormControl: boolean = false;
+    @Input() placeHolder: string = '';
+    @Input() prefix?: string;
+    @Input() suffix?: string;
 }
