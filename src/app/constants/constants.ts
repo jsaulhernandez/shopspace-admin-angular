98aac1bd1ff19f3e664324fc00c239f30e1dfdf9
Sony <jsaulhernandez98@gmail.com>
2023-07-07 18:47:35 -0600

    tag: adding new types for custom text

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,2 +1,16 @@
-export declare type TypeButton = 'primary' | 'default' | 'dashed' | 'text' | 'link';
+export declare type TypeButton =
+    | 'primary'
+    | 'default'
+    | 'dashed'
+    | 'text'
+    | 'link';
 export declare type ThemeTypeIcon = 'fill' | 'outline' | 'twotone';
+export declare type TypeTextElement =
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'h4'
+    | 'h5'
+    | 'span'
+    | 'p';
+export declare type TypeText = 'secondary' | 'success' | 'warning' | 'danger';
