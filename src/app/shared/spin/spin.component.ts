01cc2ae230720efc9b2c6f49225db70f5f91af27
Sony <jsaulhernandez98@gmail.com>
2023-07-08 17:09:40 -0600

    tag: updating code style

--- src/app/shared/spin/spin.component.ts
+++ src/app/shared/spin/spin.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-spin',
-  templateUrl: './spin.component.html',
-  styleUrls: ['./spin.component.scss']
+    selector: 'app-spin',
+    templateUrl: './spin.component.html',
+    styleUrls: ['./spin.component.scss'],
 })
-export class SpinComponent {
-
-}
+export class SpinComponent {}
