de636f151a63415090365c24f506dbc81f81789f
Sony <jsaulhernandez98@gmail.com>
2023-07-08 19:32:12 -0600

    tag: loading data in categories component

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { CategoryModel } from 'src/app/data/models/Category.interface';
+import { Component, OnInit, Type, inject } from '@angular/core';
+
 import { CategoryService } from 'src/app/services/category.service';
+import { CategoryModel } from 'src/app/data/models/Category.interface';
+import { SButtonComponent } from 'src/app/components/s-button/s-button.component';
+
+interface CustomHeader<T extends Object> {
+    title: string;
+    dataIndex?: string;
+    render?: Type<any>;
+}
 
 @Component({
     selector: 'app-category',
@@ -10,11 +18,45 @@ import { CategoryService } from 'src/app/services/category.service';
 export class CategoryComponent implements OnInit {
     _categoryService = inject(CategoryService);
 
+    isLoading: boolean = false;
+    categories: CategoryModel[] = [];
+
+    customHeader: CustomHeader<CategoryModel>[] = [
+        {
+            title: 'Nombre',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Estado',
+            dataIndex: 'status',
+        },
+        {
+            title: 'Acciones',
+            render: SButtonComponent,
+        },
+    ];
+
     ngOnInit(): void {
+        this.getCategories();
+    }
+
+    async getCategories() {
+        this.isLoading = true;
         this._categoryService
-            .getAllCategories<CategoryModel>({
+            .getAllCategories<CategoryModel[]>({
                 path: 'category/paged',
             })
-            .subscribe((res) => console.log('res', res));
+            .subscribe({
+                next: (c) => (this.categories = c.data || []),
+                error: (e) => {
+                    this.isLoading = false;
+                    this.categories = [];
+                },
+                complete: () => (this.isLoading = false),
+            });
+    }
+
+    getValueByKey(data: CategoryModel, key: string): any {
+        return data[key as keyof typeof data];
     }
 }
--- src/app/pages/category/category.module.ts
+++ src/app/pages/category/category.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+
 import { CategoryRoutingModule } from './category-routing.module';
 import { CategoryComponent } from './category.component';
 
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
-  declarations: [
-    CategoryComponent
-  ],
-  imports: [
-    CommonModule,
-    CategoryRoutingModule
-  ]
+    declarations: [CategoryComponent],
+    imports: [
+        CommonModule,
+        CategoryRoutingModule,
+        ComponentsModule,
+        NzTableModule,
+        NzSwitchModule,
+    ],
 })
-export class CategoryModule { }
+export class CategoryModule {}
