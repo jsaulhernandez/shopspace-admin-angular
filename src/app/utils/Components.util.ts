38259efae813a47e494b4ba6554bcb1d4fe31acb
Sony <jsaulhernandez98@gmail.com>
2023-07-09 17:18:12 -0600

    tag: updating header for component

--- src/app/utils/Components.util.ts
+++ src/app/utils/Components.util.ts
@@ -1,7 +1,14 @@
-import { Type } from '@angular/core';
+import { TypeElementTable } from '../constants/constants';
 
 export interface CustomHeader {
     title: string;
+    //only plain string
     dataIndex?: string;
-    render?: Type<any>;
+    render?: (data: any) => string;
+    //only buttons, switches, radios y checkboxes
+    element?: TypeElementTable;
+    onClickElement?: (data: any, index: number) => void; //evento para los elementos
+    onSecondClickElement?: (data: any, index: number) => void; //evento para los elementos
+    //only selects. Example for internal object ["id", "name"]
+    dataArray?: string[];
 }
