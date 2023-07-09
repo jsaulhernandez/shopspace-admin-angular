bd3f0a0fcb21e4172d50c825c43d2949d91042d8
Sony <jsaulhernandez98@gmail.com>
2023-07-08 19:30:52 -0600

    tag: updating imports for components in auth.module

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -2,34 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzTypographyModule } from 'ng-zorro-antd/typography';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-
 import { LoginComponent } from './login/login.component';
 
-import { SButtonComponent } from 'src/app/components/s-button/s-button.component';
-import { SInputComponent } from 'src/app/components/s-input/s-input.component';
-import { STextComponent } from 'src/app/components/s-text/s-text.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
-    declarations: [
-        LoginComponent,
-        STextComponent,
-        SButtonComponent,
-        SInputComponent,
-    ],
+    declarations: [LoginComponent],
     imports: [
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
         NzFormModule,
-        NzTypographyModule,
-        NzInputModule,
-        NzButtonModule,
-        NzIconModule,
+        ComponentsModule,
     ],
 })
 export class AuthModule {}
