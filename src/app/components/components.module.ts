95899a50f77fec235e748ba664d176f5d0ed964c
Sony <jsaulhernandez98@gmail.com>
2023-07-09 18:14:21 -0600

    tag: adding support for action in custom table

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { SButtonComponent } from './s-button/s-button.component';
 import { SInputComponent } from './s-input/s-input.component';
 import { STextComponent } from './s-text/s-text.component';
 import { STableComponent } from './s-table/s-table.component';
+import { SActionsComponent } from './s-actions/s-actions.component';
 
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
 
 @NgModule({
     declarations: [
@@ -18,6 +21,7 @@ import { NzTableModule } from 'ng-zorro-antd/table';
         SInputComponent,
         STextComponent,
         STableComponent,
+        SActionsComponent,
     ],
     imports: [
         CommonModule,
@@ -26,6 +30,8 @@ import { NzTableModule } from 'ng-zorro-antd/table';
         NzButtonModule,
         NzIconModule,
         NzTableModule,
+        NzSwitchModule,
+        FormsModule,
     ],
     exports: [
         SButtonComponent,
--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -3,8 +3,6 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CategoryService } from 'src/app/services/category.service';
 import { CategoryModel } from 'src/app/data/models/Category.interface';
 
-import { SButtonComponent } from 'src/app/components/s-button/s-button.component';
-
 import { CustomHeader } from 'src/app/utils/components.util';
 
 @Component({
@@ -26,10 +24,14 @@ export class CategoryComponent implements OnInit {
         {
             title: 'Estado',
             dataIndex: 'status',
+            element: 'switch',
+            onClickElement: (data, _) => this.onUpdate(data),
         },
         {
             title: 'Acciones',
-            render: SButtonComponent,
+            element: 'actions',
+            onClickElement: (data, _) => this.onUpdate(data),
+            onSecondClickElement: (data, _) => this.onDelete(data),
         },
     ];
 
@@ -51,5 +53,13 @@ export class CategoryComponent implements OnInit {
                 },
                 complete: () => (this.isLoading = false),
             });
+    }
+
+    onUpdate(data: CategoryModel) {
+        console.log('update', data);
+    }
+
+    onDelete(data: CategoryModel) {
+        console.log('remove', data);
     }
 }
