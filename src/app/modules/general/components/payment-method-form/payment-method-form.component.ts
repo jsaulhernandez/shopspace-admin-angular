bf715d54c30e1d00441ff5f18b0ebfc11e021f3c
Sony <jsaulhernandez98@gmail.com>
2023-08-26 20:31:10 -0600

    tag: adding component for payment methods

---
+++ src/app/modules/general/components/payment-method-form/payment-method-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-method-form',
+  templateUrl: './payment-method-form.component.html',
+  styleUrls: ['./payment-method-form.component.scss']
+})
+export class PaymentMethodFormComponent {
+
+}
