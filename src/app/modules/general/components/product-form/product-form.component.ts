531bc41aa1c8acfb24311f3dc7a7515a44a0b329
Sony <jsaulhernandez98@gmail.com>
2023-09-01 18:37:12 -0600

    tag: adding crud for product detail

--- src/app/modules/general/components/product-form/product-form.component.ts
+++ src/app/modules/general/components/product-form/product-form.component.ts
@@ -5,6 +5,8 @@ import {
     OnDestroy,
     OnInit,
     Output,
+    TemplateRef,
+    ViewChild,
 } from '@angular/core';
 import {
     UntypedFormBuilder,
@@ -21,9 +23,14 @@ import { ViewProductModel } from 'src/app/data/models/ViewProduct.model';
 import { AdminApiService } from 'src/app/data/services/core/admin-api.service';
 import { LoaderService } from 'src/app/shared/services/loader.service';
 import { NotificationService } from 'src/app/shared/services/notification.service';
+import { ModalService } from 'src/app/shared/services/modal.service';
 
 import { FormUtils } from 'src/app/core/utils/form.util';
 import { CustomHeader } from 'src/app/core/utils/components.util';
+import { UserActions } from 'src/app/data/constants/constants';
+
+type optionsTemplate = 'detail' | 'view';
+type optionsModal = 'open' | 'close';
 
 @Component({
     selector: 'app-product-form',
@@ -35,11 +42,21 @@ export class ProductFormComponent implements OnInit, OnDestroy {
     @Output() onCancel = new EventEmitter();
     @Output() submitted = new EventEmitter<ProductModel>();
 
+    //templates to modal
+    @ViewChild('templateDetail') detailTemplate!: TemplateRef<any>;
+    @ViewChild('templateView') viewTemplate!: TemplateRef<any>;
+
     validateForm!: UntypedFormGroup;
+    detailForm!: UntypedFormGroup;
+    viewForm!: UntypedFormGroup;
     productDetails: ProductDetailModel[] = [];
     viewsProduct: ViewProductModel[] = [];
+    auxDetail!: ProductDetailModel | undefined;
+    auxView!: ViewProductModel | undefined;
+    position!: number | undefined;
 
     isLoading = this.loader$.loading$;
+    action: UserActions = 'save';
     brands: BrandModel[] = [];
     subscriber!: Subscription;
 
@@ -68,7 +85,8 @@ export class ProductFormComponent implements OnInit, OnDestroy {
         private fb: UntypedFormBuilder,
         private api$: AdminApiService,
         private loader$: LoaderService,
-        private notification$: NotificationService
+        private notification$: NotificationService,
+        private modal$: ModalService
     ) {}
 
     ngOnInit(): void {
@@ -93,6 +111,21 @@ export class ProductFormComponent implements OnInit, OnDestroy {
             brand: this.fb.group({
                 id: [this.data?.brand.id ?? null, [Validators.required]],
             }),
+        });
+
+        this.detailForm = this.fb.group({
+            id: [null],
+            identifier: [null, [Validators.required]],
+            value: [null, [Validators.required]],
+            productId: [null],
+        });
+
+        this.viewForm = this.fb.group({
+            id: [null],
+            color: [null, [Validators.required]],
+            stock: [null, [Validators.required]],
+            status: [null, [Validators.required]],
+            productId: [null],
         });
 
         this.getBrands();
@@ -123,6 +156,135 @@ export class ProductFormComponent implements OnInit, OnDestroy {
                 },
                 complete: () => this.loader$.hide(),
             });
+    }
+
+    onCustomModal(
+        option: optionsTemplate = 'detail',
+        action: optionsModal = 'open',
+        method: UserActions = 'save'
+    ) {
+        if (action === 'open') {
+            this.action = method;
+
+            if (option === 'detail') {
+                if (method === 'update')
+                    this.detailForm.patchValue(this.auxDetail ?? {});
+                else {
+                    this.resetDefaultValues();
+                }
+
+                this.modal$.open(this.detailTemplate, {
+                    title: 'Add detail',
+                });
+            } else {
+                if (method === 'update')
+                    this.viewForm.patchValue(this.auxView ?? {});
+                else {
+                    this.resetDefaultValues('view');
+                    this.viewForm.patchValue({ status: '1' });
+                }
+
+                this.modal$.open(this.viewTemplate, {
+                    title: 'Add view',
+                });
+            }
+        } else {
+            this.resetDefaultValues(option);
+            this.modal$.close();
+        }
+    }
+
+    //Method for product detail
+    submitDetailForm(): void {
+        if (!this.detailForm.valid) {
+            FormUtils.invalidate(this.detailForm);
+            return;
+        }
+
+        const dataForm = this.detailForm.getRawValue();
+        let exists: boolean = false;
+
+        if (this.auxDetail && this.action === 'update') {
+            if (this.auxDetail.identifier !== dataForm.identifier)
+                exists = this.existsData('detail', dataForm);
+        } else exists = this.existsData('detail', dataForm);
+
+        if (exists) {
+            this.notification$.onNotification('error', 'El registro ya existe');
+            return;
+        }
+
+        if (this.action === 'save') this.productDetails.push(dataForm);
+        else this.updateData('detail', dataForm);
+
+        this.onCustomModal('detail', 'close');
+    }
+
+    onUpdateDetail(data: ProductDetailModel, index: number) {
+        this.auxDetail = data;
+        this.position = index;
+        this.onCustomModal('detail', 'open', (this.action = 'update'));
+    }
+
+    onRemoveDetail(index: number) {
+        this.productDetails.splice(index, 1);
+    }
+
+    //methods for view product
+    submitViewForm(): void {
+        if (!this.viewForm.valid) {
+            FormUtils.invalidate(this.viewForm);
+            return;
+        }
+
+        this.viewsProduct.push(this.viewForm.getRawValue());
+    }
+
+    //another methods
+    existsData(
+        option: optionsTemplate = 'detail',
+        data: ProductDetailModel | ViewProductModel
+    ): boolean {
+        if (option === 'detail') {
+            const detail = data as ProductDetailModel;
+            const result = this.productDetails.find(
+                (d) => d.identifier === detail.identifier
+            );
+
+            return !!result;
+        }
+
+        return false;
+    }
+
+    updateData(
+        option: optionsTemplate = 'detail',
+        data: ProductDetailModel | ViewProductModel
+    ) {
+        if (option === 'detail') {
+            this.productDetails = this.productDetails.map((p, index) => {
+                if (this.position === index) return data as ProductDetailModel;
+                return p;
+            });
+        } else {
+            this.viewsProduct = this.viewsProduct.map((p, index) => {
+                if (this.position === index) return data as ViewProductModel;
+                return p;
+            });
+        }
+    }
+
+    resetDefaultValues(option: optionsTemplate = 'detail') {
+        this.action === 'save';
+        this.position = undefined;
+
+        if (option === 'detail') {
+            this.auxDetail = undefined;
+            this.detailForm.reset();
+        } else {
+            this.auxView = undefined;
+            this.viewForm.reset();
+        }
     }
 
     submitForm(): void {
