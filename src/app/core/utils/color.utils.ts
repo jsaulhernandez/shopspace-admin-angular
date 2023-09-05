dfb34b6953bcdb8aea619c8b8e711e5008ec9256
Sony <jsaulhernandez98@gmail.com>
2023-09-05 15:44:50 -0600

    tag: adding method to convert rgba to hex

--- src/app/core/utils/color.utils.ts
+++ src/app/core/utils/color.utils.ts
@@ -13,4 +13,21 @@ export class Color {
 
         return `rgba(${r},${g},${b},${opacity})`;
     }
+
+    public static rgbaToHex(value: string, forceRemoveAlpha = false) {
+        return (
+            '#' +
+            value
+                .replace(/^rgba?\(|\s+|\)$/g, '') // Get's rgba / rgb string values
+                .split(',') // splits them at ","
+                .filter((string, index) => !forceRemoveAlpha || index !== 3)
+                .map((string) => parseFloat(string)) // Converts them to numbers
+                .map((number, index) =>
+                    index === 3 ? Math.round(number * 255) : number
+                ) // Converts alpha to 255 number
+                .map((number) => number.toString(16)) // Converts numbers to hex
+                .map((string) => (string.length === 1 ? '0' + string : string)) // Adds 0 when length of one number is 1
+                .join('')
+        );
+    }
 }
