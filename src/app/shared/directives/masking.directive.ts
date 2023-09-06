223a339edf964b58902821a07e19dc76cf559d9b
Sony <jsaulhernandez98@gmail.com>
2023-09-06 17:12:54 -0600

    tag: finishing directive for masking

--- src/app/shared/directives/masking.directive.ts
+++ src/app/shared/directives/masking.directive.ts
@@ -1,10 +1,46 @@
-import { Directive } from '@angular/core';
+import {
+    Directive,
+    ElementRef,
+    HostListener,
+    Input,
+    OnInit,
+    Renderer2,
+} from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Directive({
-  selector: '[appMasking]'
+    selector: '[appMasking]',
 })
-export class MaskingDirective {
+export class MaskingDirective implements OnInit {
+    @Input() appMaskValue?: RegExp;
+    private specialKeys = [
+        'Backspace',
+        'Delete',
+        'Tab',
+        'End',
+        'Home',
+        'ArrowLeft',
+        'ArrowRight',
+    ];
 
-  constructor() { }
+    constructor(
+        private elRef: ElementRef,
+        private renderer: Renderer2,
+        private ngModel: NgModel
+    ) {}
 
+    ngOnInit(): void {}
+
+    @HostListener('keydown', ['$event'])
+    onKeyDown(event: KeyboardEvent) {
+        if (this.appMaskValue) {
+            if (this.specialKeys.indexOf(event.key) !== -1) return;
+
+            let current: string = this.elRef.nativeElement.value;
+            let next: string = current.concat(event.key);
+
+            if (next && !String(next).match(this.appMaskValue))
+                event.preventDefault();
+        }
+    }
 }
