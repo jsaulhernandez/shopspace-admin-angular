66faa6b3bba844f7d95d0544e1cfa01aebc466f6
Sony <jsaulhernandez98@gmail.com>
2023-09-06 17:13:10 -0600

    tag: adding new RegEx for directives

--- src/app/core/utils/RegEx.utils.ts
+++ src/app/core/utils/RegEx.utils.ts
@@ -1,5 +1,8 @@
 export const OnlyNumbersRegEx = /^[0-9]+$/;
 export const OnlyDecimalsNumberSRegEx = /^[0-9]+([.][0-9]{1,2})?$/;
+export const OnlyDecimalsNumbersWithPointRegEx =
+    /^[0-9]+((\.{1})([\d]{1,2})?)?$/; // only use in directives
+export const OnlyNumbersWithOneDotRegEx = /[^0-9.]+/g; //only use in directive currency
 
 export const ValidationEmail =
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
