c4f99cb4b9b79f66a8834a4fa5fcc1eee17f14f7
Sony <jsaulhernandez98@gmail.com>
2023-09-22 14:23:54 -0600

    tag: switching to promise method to get base64

--- src/app/core/utils/file.utils.ts
+++ src/app/core/utils/file.utils.ts
@@ -1,12 +1,23 @@
 export class FileUtils {
-    public static getBase64(
-        file: File,
-        callback: (base64: string) => void
-    ): void {
-        const reader = new FileReader();
-        reader.addEventListener('load', () =>
-            callback(reader.result!.toString())
-        );
-        reader.readAsDataURL(file);
+    public static getBase64(file: File): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.addEventListener('load', () =>
+                resolve(reader.result!.toString())
+            );
+            reader.addEventListener('error', () =>
+                reject(
+                    new Error('An error occurred when trying to get the base64')
+                )
+            );
+        });
+    }
+
+    public static isBase64(str?: string | null): boolean {
+        if (!str) return false;
+        if (str === '' || str.trim() === '') return false;
+
+        return str.includes('data:image');
     }
 }
