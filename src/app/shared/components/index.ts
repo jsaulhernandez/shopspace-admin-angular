a4ed10f30897f087e84da8042461d62c1f34ea5e
Sony <jsaulhernandez98@gmail.com>
2023-09-22 14:27:21 -0600

    tag: creating component to show images

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -8,6 +8,7 @@ import { SEditorComponent } from './s-editor/s-editor.component';
 import { SInputCouponCodeComponent } from './s-input-coupon-code/s-input-coupon-code.component';
 import { SInputComponent } from './s-input/s-input.component';
 import { SModalActionsComponent } from './s-modal-actions/s-modal-actions.component';
+import { SPreviewImageComponent } from './s-preview-image/s-preview-image.component';
 import { STableComponent } from './s-table/s-table.component';
 import { STextComponent } from './s-text/s-text.component';
 import { SUploadFileComponent } from './s-upload-file/s-upload-file.component';
@@ -28,4 +29,5 @@ export const SHARED_COMPONENTS = [
     SInputCouponCodeComponent,
     SEditorComponent,
     SUploadFileComponent,
+    SPreviewImageComponent,
 ];
---
+++ src/app/shared/components/s-preview-image/s-preview-image.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-s-preview-image',
+  templateUrl: './s-preview-image.component.html',
+  styleUrls: ['./s-preview-image.component.scss']
+})
+export class SPreviewImageComponent {
+
+}
