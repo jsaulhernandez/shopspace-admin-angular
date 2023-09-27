ea1771e10d566c5fc2ad62bd30a5bebb84550f8f
Sony <jsaulhernandez98@gmail.com>
2023-09-27 16:51:21 -0600

    tag: adding new field to upload image

--- src/app/data/models/Brand.model.ts
+++ src/app/data/models/Brand.model.ts
@@ -1,5 +1,6 @@
 export interface BrandModel {
     id?: number;
     name: string;
+    image: string;
     status: number;
 }
--- src/app/data/models/Category.model.ts
+++ src/app/data/models/Category.model.ts
@@ -1,5 +1,6 @@
 export interface CategoryModel {
     id?: number;
     name: string;
+    image: string;
     status: number;
 }
